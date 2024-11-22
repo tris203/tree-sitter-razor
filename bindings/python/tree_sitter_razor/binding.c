2bfd222f5bad1c6d9cee55bf0a7d55692c07269b
tris203 <admin@snappeh.com>
2024-11-22 16:53:23 +0000

    git lfs config

--- bindings/python/tree_sitter_razor/binding.c
+++ bindings/python/tree_sitter_razor/binding.c
@@ -1,3 +1,27 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1a791088483972028c49c29f90a8a1abe04f15efae02901b2e1a2e2f36b6e2e6
-size 681
+#include <Python.h>
+
+typedef struct TSLanguage TSLanguage;
+
+TSLanguage *tree_sitter_razor(void);
+
+static PyObject* _binding_language(PyObject *Py_UNUSED(self), PyObject *Py_UNUSED(args)) {
+    return PyCapsule_New(tree_sitter_razor(), "tree_sitter.Language", NULL);
+}
+
+static PyMethodDef methods[] = {
+    {"language", _binding_language, METH_NOARGS,
+     "Get the tree-sitter language for this grammar."},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef module = {
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "_binding",
+    .m_doc = NULL,
+    .m_size = -1,
+    .m_methods = methods
+};
+
+PyMODINIT_FUNC PyInit__binding(void) {
+    return PyModule_Create(&module);
+}
